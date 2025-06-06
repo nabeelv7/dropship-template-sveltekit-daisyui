a27c36cb16c7f63297c51a9e7e8af08ce83eb545
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-06-06 16:20:17 +0500

    done

--- src/lib/components/CTA.svelte
+++ src/lib/components/CTA.svelte
@@ -7,31 +7,33 @@
   ];
 </script>
 
-<div class="py-50 flex items-center justify-center px-5">
-  <div class="max-w-screen-lg text-center flex flex-col gap-2">
-    <h1 class="text-3xl font-bold">Buy Now</h1>
-    <p class="text-opacity-80 text-gray-600">
-      We are trusted by hundreds of happy customers
-    </p>
+<div class="py-20 flex items-center justify-center px-5">
+  <div class="bg-base-100/60 border border-primary rounded-box p-10">
+    <div class="max-w-screen-lg text-center flex flex-col gap-2">
+      <h1 class="text-3xl font-bold">Buy Now</h1>
+      <p class="text-opacity-80 text-gray-600">
+        We are trusted by hundreds of happy customers
+      </p>
 
-    <div class="flex justify-center gap-1">
-      <img src="star.svg" alt="Star" class="h-6 w-6" />
-      <img src="star.svg" alt="Star" class="h-6 w-6" />
-      <img src="star.svg" alt="Star" class="h-6 w-6" />
-      <img src="star.svg" alt="Star" class="h-6 w-6" />
-      <img src="star.svg" alt="Star" class="h-6 w-6" />
-    </div>
+      <div class="flex justify-center gap-1">
+        <img src="star.svg" alt="Star" class="h-6 w-6" />
+        <img src="star.svg" alt="Star" class="h-6 w-6" />
+        <img src="star.svg" alt="Star" class="h-6 w-6" />
+        <img src="star.svg" alt="Star" class="h-6 w-6" />
+        <img src="star.svg" alt="Star" class="h-6 w-6" />
+      </div>
 
-    <div class="avatar-group -space-x-6 flex justify-center">
-      {#each avatars as avatar}
-        <div class="avatar">
-          <div class="w-12">
-            <img src={avatar} alt="Happy customer avatar" />
+      <div class="avatar-group -space-x-6 flex justify-center">
+        {#each avatars as avatar}
+          <div class="avatar">
+            <div class="w-12">
+              <img src={avatar} alt="Happy customer avatar" />
+            </div>
           </div>
-        </div>
-      {/each}
-    </div>
+        {/each}
+      </div>
 
-    <a href="#products" class="btn btn-primary mt-6">View Products</a>
+      <a href="#products" class="btn btn-primary mt-6">View Products &rarr;</a>
+    </div>
   </div>
 </div>
