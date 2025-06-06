7cecc7ce98d4d731f5671ea2597a84d06689c6e8
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-06-06 15:04:15 +0500

    done 🎉

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,28 +1,30 @@
-<footer
-  class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 mt-10"
->
-  <aside>
-    <img src="icon.png" alt="logo" class="h-10 w-10" />
-    <p class="text-3xl font-extrabold">
-      daisyUI
-      <br />
-    </p>
-  </aside>
-  <div class="grid grid-cols-2 w-full">
-    <nav class="flex flex-col">
-      <h6 class="footer-title">Products</h6>
-      <a href="/" class="link link-hover">Product 1</a>
-      <a href="/" class="link link-hover">Product 2</a>
-      <a href="/" class="link link-hover">Product 3</a>
-      <a href="/" class="link link-hover">Product 4</a>
-      <a href="/" class="link link-hover">Product 5</a>
-      <a href="/" class="link link-hover">Product 6</a>
-    </nav>
-    <nav class="flex flex-col">
-      <h6 class="footer-title">Site</h6>
-      <a class="link link-hover" href="#products">Products</a>
-      <a class="link link-hover" href="#testimonials">Testimonials</a>
-      <a class="link link-hover" href="#faq">FAQ</a>
-    </nav>
-  </div>
-</footer>
+<div class="bg-neutral">
+  <footer
+    class="footer max-w-screen-lg mx-auto sm:footer-horizontal bg-neutral text-neutral-content p-10 mt-10"
+  >
+    <aside>
+      <img src="icon.png" alt="logo" class="h-10 w-10" />
+      <p class="text-3xl font-extrabold">
+        daisyUI
+        <br />
+      </p>
+    </aside>
+    <div class="grid grid-cols-2 w-full">
+      <nav class="flex flex-col gap-2">
+        <h6 class="footer-title">Products</h6>
+        <a href="/" class="link link-hover">Product 1</a>
+        <a href="/" class="link link-hover">Product 2</a>
+        <a href="/" class="link link-hover">Product 3</a>
+        <a href="/" class="link link-hover">Product 4</a>
+        <a href="/" class="link link-hover">Product 5</a>
+        <a href="/" class="link link-hover">Product 6</a>
+      </nav>
+      <nav class="flex flex-col gap-2">
+        <h6 class="footer-title">Site</h6>
+        <a class="link link-hover" href="#products">Products</a>
+        <a class="link link-hover" href="#testimonials">Testimonials</a>
+        <a class="link link-hover" href="#faq">FAQ</a>
+      </nav>
+    </div>
+  </footer>
+</div>
