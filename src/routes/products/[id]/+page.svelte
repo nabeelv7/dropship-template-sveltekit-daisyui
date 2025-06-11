08ad35f49251d3f64a5a47cc40919c91a0fb8f8c
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-06-11 15:32:29 +0500

    mann

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section
-  class=" mx-auto md:py-30 py-10 px-5 grid md:grid-cols-2 grid-cols-1 gap-10 min-h-screen"
+  class="mx-auto md:py-30 py-10 px-5 grid md:grid-cols-2 grid-cols-1 gap-10 min-h-screen"
 >
   <Carousel />
   <ProductText />
